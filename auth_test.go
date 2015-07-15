e34c676b8d14d2aee2b8d712562151ba04625be5
Alex Mayfield <alexmax2742@gmail.com>
2015-07-14 20:22:09 -0400

    Add license to files, plus update copyright dates.

--- auth_test.go
+++ auth_test.go
@@ -1,6 +1,6 @@
 /*
  *  Charon: A game authentication server
- *  Copyright (C) 2014  Alex Mayfield <alexmax2742@gmail.com>
+ *  Copyright (C) 2014-2015  Alex Mayfield <alexmax2742@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published by
